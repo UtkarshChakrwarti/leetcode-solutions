de9c6816370db539c333c143fd756f527c817c84
Utkarsh Chakrwarti <76407507+UtkarshChakrwarti@users.noreply.github.com>
2022-09-11 22:17:06 +0530

    Create 724. Find Pivot Index.py

---
+++ 724. Find Pivot Index.py
@@ -0,0 +1,10 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        total = sum(nums)
+
+        for i in range(len(nums)):
+            left = sum(nums[:i])
+
+            if left == total-left-nums[i]:
+                return(i)
+        return -1
